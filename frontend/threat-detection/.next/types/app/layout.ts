3a898c1dad6a8c6fa3eac567c3ba04194f663186
root <root@threat-detection-server>
2025-06-09 13:56:45 +0000

    otimizaçoes 1

--- frontend/threat-detection/.next/types/app/layout.ts
+++ frontend/threat-detection/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/victor/Documentos/NGSX/frontend/threat-detection/src/app/layout.js
+// File: /root/threat-detection/frontend/threat-detection/src/app/layout.js
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- frontend/threat-detection/.next/types/app/page.ts
+++ frontend/threat-detection/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/victor/Documentos/NGSX/frontend/threat-detection/src/app/page.js
+// File: /root/threat-detection/frontend/threat-detection/src/app/page.js
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
